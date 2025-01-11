f344d22581143862a08fadbe87ed4c4fc1f9ac14
thambithuthi <thambithuthi@gmail.com>
2025-01-11 23:11:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -89,7 +89,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/thambithuthimaingrp')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_botz')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/thambithuthichannel')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/kingvj01')
 
